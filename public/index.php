745a60342603edce7024fa3f1d3274e02a6b39c3
[Merhene] <merwan.st@gmail.com>
2023-10-21 21:29:32 +0200

    changed for the quest

--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
+require __DIR__ . '/../public/products.php';
 
 
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['name' => $name, 'products' => $products]);
---
+++ public/products.php
@@ -0,0 +1,5 @@
+<?php
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+?>
